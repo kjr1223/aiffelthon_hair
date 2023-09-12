d581a9ec26a3faa7a18b3db259a91e32d7a444f6
KIMTAEWON <guineapig7351@gmail.com>
2023-09-12 12:28:18 +0900

    모델6개탑재2

--- aiffelthon_hair/lib/main.dart
+++ aiffelthon_hair/lib/main.dart
@@ -1,15 +1,16 @@
-import 'package:aiffelthon_hair/services/auth_wrapper.dart';
+// import 'package:aiffelthon_hair/services/auth_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:aiffelthon_hair/ui_screen/navigation_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-    home: AuthenticationWrapper(),
+  runApp(const MaterialApp(
+    home: NavigationScreen(),
   ));
 }
 
